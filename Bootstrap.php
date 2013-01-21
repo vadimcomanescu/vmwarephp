6ef537b9c7079407c98365a39a61df787dd55f2c
Vadim Comanescu <vadim984@gmail.com>
2013-01-22 01:20:53 +0200

    Test bootstrap now loads php Mockery.

--- Bootstrap.php
+++ Bootstrap.php
@@ -1,19 +1,33 @@
 <?php
 error_reporting(E_ALL | E_STRICT);
 
-$root    = realpath(dirname(__FILE__));
-$library = "$root/library";
-$tests   = "$root/tests";
-var_dump($library);
+function includeProjectInPath() {
+    $root    = realpath(dirname(__FILE__));
+    $library = "$root/library";
+    $tests   = "$root/tests";
 
-$path = array(
-    $library,
-    $tests,
-    get_include_path(),
-);
-set_include_path(implode(PATH_SEPARATOR, $path));
+    $path = array(
+        $library,
+        $tests,
+        get_include_path(),
+    );
+    set_include_path(implode(PATH_SEPARATOR, $path));
+}
 
+function loadMockery() {
+    require_once 'Mockery/Loader.php';
+    require_once 'Hamcrest/Hamcrest.php';
 
-require_once 'Vmwarephp/Autoloader.php';
-$autoloader = new Autoloader();
-$autoloader->register();
+    $loader = new Mockery\Loader;
+    $loader->register();
+}
+
+function enableProjectClassAutoloader() {
+    require_once 'Vmwarephp/Autoloader.php';
+    $autoloader = new \Vmwarephp\Autoloader;
+    $autoloader->register();
+}
+
+includeProjectInPath();
+loadMockery();
+enableProjectClassAutoloader();
