2ab62c07a165a12a267db1056d1a5846c8c1a30b
Vadim Comanescu <vadim984@gmail.com>
2013-01-23 01:11:32 +0200

    Reformat in wsdl class mapper and test

--- library/Vmwarephp/WsdlClassMapper.php
+++ library/Vmwarephp/WsdlClassMapper.php
@@ -1,77 +1,76 @@
 <?php
 namespace Vmwarephp;
 
-class WsdlClassMapper
-{
-    private $classDefinitionsFilePath;
-    private $classMap;
-    private $useClassMapCaching = true;
+class WsdlClassMapper {
+	private $classDefinitionsFilePath;
+	private $classMap;
+	private $useClassMapCaching = true;
 
-    function __construct($classDefinitionsFilePath = null) {
-        $this->classDefinitionsFilePath = $classDefinitionsFilePath ? : dirname(__FILE__) . '/TypeDefinitions.inc';
-    }
+	function __construct($classDefinitionsFilePath = null) {
+		$this->classDefinitionsFilePath = $classDefinitionsFilePath ? : dirname(__FILE__) . '/TypeDefinitions.inc';
+	}
 
-    function getClassMap() {
-        if (!empty($this->classMap)) return $this->classMap;
-        $this->initializeClassMap();
-        return $this->classMap;
-    }
+	function getClassMap() {
+		if (!empty($this->classMap)) return $this->classMap;
+		$this->initializeClassMap();
+		return $this->classMap;
+	}
 
-    function configureClassMapCaching($value) {
-        $this->useClassMapCaching = $value;
-    }
+	function configureClassMapCaching($value) {
+		$this->useClassMapCaching = $value;
+	}
 
-    private function initializeClassMapFromCache() {
-        $cacheFilePath = $this->makeCacheFilePath();
-        if (!file_exists($cacheFilePath) || !$this->useClassMapCaching) return;
-        $this->classMap = unserialize(file_get_contents($cacheFilePath));
-    }
+	private function initializeClassMapFromCache() {
+		$cacheFilePath = $this->makeCacheFilePath();
+		if (!file_exists($cacheFilePath) || !$this->useClassMapCaching) return;
+		$this->classMap = unserialize(file_get_contents($cacheFilePath));
+	}
 
-    private function initializeClassMap() {
-        $this->initializeClassMapFromCache();
-        if ($this->classMap) return;
-        $this->classMap = $this->generateClassMap();
-        $this->cacheClassMap();
-    }
+	private function initializeClassMap() {
+		$this->initializeClassMapFromCache();
+		if ($this->classMap) return;
+		$this->classMap = $this->generateClassMap();
+		$this->cacheClassMap();
+	}
 
-    private function generateClassMap() {
-        $classMap = array();
-        $allTokens = token_get_all($this->readClassDefinitions());
-        $namespace = null;
-        foreach ($allTokens as $key => $token) {
-            if ($this->tokenRepresentsNamespaceDefinition($token))
-                $namespace = $allTokens[$key + 2][1];
-            if ($this->tokenRepresentsClassDefinition($token)) {
-                $className = $allTokens[$key + 2][1];
-                $classMap[$className] = $this->makeAbsoluteClassName($namespace, $className);
-            }
-        }
-        return $classMap;
-    }
+	private function generateClassMap() {
+		$classMap = array();
+		$allTokens = token_get_all($this->readClassDefinitions());
+		$namespace = null;
+		foreach ($allTokens as $key => $token) {
+			if ($this->tokenRepresentsNamespaceDefinition($token))
+				$namespace = $allTokens[$key + 2][1];
+			if ($this->tokenRepresentsClassDefinition($token)) {
+				$className = $allTokens[$key + 2][1];
+				$classMap[$className] = $this->makeAbsoluteClassName($namespace, $className);
+			}
+		}
+		return $classMap;
+	}
 
-    private function makeAbsoluteClassName($namespace, $className) {
-        return $namespace ? $namespace . '\\' . $className : $className;
-    }
+	private function makeAbsoluteClassName($namespace, $className) {
+		return $namespace ? $namespace . '\\' . $className : $className;
+	}
 
-    private function tokenRepresentsClassDefinition($token) {
-        return is_array($token) && $token[0] == T_CLASS;
-    }
+	private function tokenRepresentsClassDefinition($token) {
+		return is_array($token) && $token[0] == T_CLASS;
+	}
 
-    private function tokenRepresentsNamespaceDefinition($token) {
-        return is_array($token) && $token[0] == T_NAMESPACE;
-    }
+	private function tokenRepresentsNamespaceDefinition($token) {
+		return is_array($token) && $token[0] == T_NAMESPACE;
+	}
 
-    private function readClassDefinitions() {
-        if (!file_exists($this->classDefinitionsFilePath)) return '';
-        return file_get_contents($this->classDefinitionsFilePath);
-    }
+	private function readClassDefinitions() {
+		if (!file_exists($this->classDefinitionsFilePath)) return '';
+		return file_get_contents($this->classDefinitionsFilePath);
+	}
 
-    private function cacheClassMap() {
-        if (!$this->useClassMapCaching) return;
-        file_put_contents($this->makeCacheFilePath(), serialize($this->classMap));
-    }
+	private function cacheClassMap() {
+		if (!$this->useClassMapCaching) return;
+		file_put_contents($this->makeCacheFilePath(), serialize($this->classMap));
+	}
 
-    private function makeCacheFilePath() {
-        return dirname(__FILE__) . '/' . 'wsdl_class_map_cache';
-    }
+	private function makeCacheFilePath() {
+		return dirname(__FILE__) . '/' . '.wsdl_class_map_cache';
+	}
 }
--- tests/WsdlClassMapperTest.php
+++ tests/WsdlClassMapperTest.php
@@ -1,45 +1,44 @@
 <?php
 
-class WsdlClassMapperTest extends PHPUnit_Framework_TestCase
-{
+class WsdlClassMapperTest extends PHPUnit_Framework_TestCase {
 
-    private $wsdlClassMapper;
-    private static $classDefinitionsFile;
+	private $wsdlClassMapper;
+	private static $classDefinitionsFile;
 
-    static function setUpBeforeClass() {
-        self::$classDefinitionsFile = sprintf('/tmp/' . uniqid());
-    }
+	static function setUpBeforeClass() {
+		self::$classDefinitionsFile = sprintf('/tmp/' . uniqid());
+	}
 
-    static function tearDownAfterClass() {
-        @unlink(self::$classDefinitionsFile);
-    }
+	static function tearDownAfterClass() {
+		@unlink(self::$classDefinitionsFile);
+	}
 
-    protected function setUp() {
-        $this->wsdlClassMapper = new Vmwarephp\WsdlClassMapper(self::$classDefinitionsFile);
-        $this->wsdlClassMapper->configureClassMapCaching(false);
-    }
+	protected function setUp() {
+		$this->wsdlClassMapper = new Vmwarephp\WsdlClassMapper(self::$classDefinitionsFile);
+		$this->wsdlClassMapper->configureClassMapCaching(false);
+	}
 
-    function testCanProvideAClassMapWhenWsdlClassDefinitionsAreProvided() {
-        $this->writeWsdlClassDefinitions("<?php namespace Vmwarephp;\n class Klass {} \n class Klass1 {}");
+	function testCanProvideAClassMapWhenWsdlClassDefinitionsAreProvided() {
+		$this->writeWsdlClassDefinitions("<?php namespace Vmwarephp;\n class Klass {} \n class Klass1 {}");
 
-        $expectedClassMap = array('Klass' => 'Vmwarephp\Klass', 'Klass1' => 'Vmwarephp\Klass1');
-        $this->assertEquals($expectedClassMap, $this->wsdlClassMapper->getClassMap());
-    }
+		$expectedClassMap = array('Klass' => 'Vmwarephp\Klass', 'Klass1' => 'Vmwarephp\Klass1');
+		$this->assertEquals($expectedClassMap, $this->wsdlClassMapper->getClassMap());
+	}
 
-    function testClassMapIsEmptyWhenClassDefinitionsAreNotInAPhpFile() {
-        $this->writeWsdlClassDefinitions("class Klass {} \n class Klass1 {}");
+	function testClassMapIsEmptyWhenClassDefinitionsAreNotInAPhpFile() {
+		$this->writeWsdlClassDefinitions("class Klass {} \n class Klass1 {}");
 
-        $this->assertEmpty($this->wsdlClassMapper->getClassMap());
-    }
+		$this->assertEmpty($this->wsdlClassMapper->getClassMap());
+	}
 
-    function testClassMapDoesNotContainDuplicates() {
-        $this->writeWsdlClassDefinitions("<?php namespace Vmwarephp;\n class Klass {} \n class Klass {}");
+	function testClassMapDoesNotContainDuplicates() {
+		$this->writeWsdlClassDefinitions("<?php namespace Vmwarephp;\n class Klass {} \n class Klass {}");
 
-        $expectedClassMap = array('Klass' => 'Vmwarephp\Klass');
-        $this->assertEquals($expectedClassMap, $this->wsdlClassMapper->getClassMap());
-    }
+		$expectedClassMap = array('Klass' => 'Vmwarephp\Klass');
+		$this->assertEquals($expectedClassMap, $this->wsdlClassMapper->getClassMap());
+	}
 
-    private function writeWsdlClassDefinitions($classDefinitions) {
-        file_put_contents(self::$classDefinitionsFile, $classDefinitions);
-    }
+	private function writeWsdlClassDefinitions($classDefinitions) {
+		file_put_contents(self::$classDefinitionsFile, $classDefinitions);
+	}
 }
