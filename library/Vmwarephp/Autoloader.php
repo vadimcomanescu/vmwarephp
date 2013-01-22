1b5f484ea98cedd41353eaf73749288e604861d7
Vadim Comanescu <vadim984@gmail.com>
2013-01-22 22:56:24 +0200

    Modify autoloader to include library path as default

--- library/Vmwarephp/Autoloader.php
+++ library/Vmwarephp/Autoloader.php
@@ -1,137 +1,81 @@
 <?php
 
-/**
- * SplClassLoader implementation that implements the technical interoperability
- * standards for PHP 5.3 namespaces and class names.
- *
- * http://groups.google.com/group/php-standards/web/final-proposal
- *
- *     // Example which loads classes for the Doctrine Common package in the
- *     // Doctrine\Common namespace.
- *     $classLoader = new SplClassLoader('Doctrine\Common', '/path/to/doctrine');
- *     $classLoader->register();
- *
- * @author Jonathan H. Wage <jonwage@gmail.com>
- * @author Roman S. Borschel <roman@code-factory.org>
- * @author Matthew Weier O'Phinney <matthew@zend.com>
- * @author Kris Wallsmith <kris.wallsmith@gmail.com>
- * @author Fabien Potencier <fabien.potencier@symfony-project.org>
- */
-
 namespace Vmwarephp;
 
-class Autoloader {
-    private $fileExtension = '.php';
-    private $namespace;
-    private $includePath;
-    private $namespaceSeparator = '\\';
+class Autoloader
+{
+	private $fileExtension = '.php';
+	private $namespace;
+	private $includePath;
+	private $namespaceSeparator = '\\';
 
-    /**
-     * Creates a new <tt>SplClassLoader</tt> that loads classes of the
-     * specified namespace.
-     *
-     * @param string $ns The namespace to use.
-     */
-    public function __construct($ns = null, $includePath = null)
-    {
-        $this->namespace = $ns;
-        $this->includePath = $includePath;
-    }
+	public function __construct($ns = 'Vmwarephp', $includePath = null) {
+		$this->namespace = $ns;
+		$this->includePath = $includePath ? : $this->getLibraryPath();
+	}
 
-    /**
-     * Sets the namespace separator used by classes in the namespace of this class loader.
-     *
-     * @param string $sep The separator to use.
-     */
-    public function setNamespaceSeparator($sep)
-    {
-        $this->namespaceSeparator = $sep;
-    }
+	public function setNamespaceSeparator($sep) {
+		$this->namespaceSeparator = $sep;
+	}
 
-    /**
-     * Gets the namespace seperator used by classes in the namespace of this class loader.
-     *
-     * @return void
-     */
-    public function getNamespaceSeparator()
-    {
-        return $this->namespaceSeparator;
-    }
+	public function getNamespaceSeparator() {
+		return $this->namespaceSeparator;
+	}
 
-    /**
-     * Sets the base include path for all class files in the namespace of this class loader.
-     *
-     * @param string $includePath
-     */
-    public function setIncludePath($includePath)
-    {
-        $this->includePath = $includePath;
-    }
+	public function setIncludePath($includePath) {
+		$this->includePath = $includePath;
+	}
 
-    /**
-     * Gets the base include path for all class files in the namespace of this class loader.
-     *
-     * @return string $includePath
-     */
-    public function getIncludePath()
-    {
-        return $this->includePath;
-    }
+	public function getIncludePath() {
+		return $this->includePath;
+	}
 
-    /**
-     * Sets the file extension of class files in the namespace of this class loader.
-     *
-     * @param string $fileExtension
-     */
-    public function setFileExtension($fileExtension)
-    {
-        $this->fileExtension = $fileExtension;
-    }
+	public function setFileExtension($fileExtension) {
+		$this->fileExtension = $fileExtension;
+	}
 
-    /**
-     * Gets the file extension of class files in the namespace of this class loader.
-     *
-     * @return string $fileExtension
-     */
-    public function getFileExtension()
-    {
-        return $this->fileExtension;
-    }
+	public function getFileExtension() {
+		return $this->fileExtension;
+	}
 
-    /**
-     * Installs this class loader on the SPL autoload stack.
-     */
-    public function register()
-    {
-        spl_autoload_register(array($this, 'loadClass'));
-    }
+	public function register() {
+		spl_autoload_register(array($this, 'loadClass'));
+	}
 
-    /**
-     * Uninstalls this class loader from the SPL autoloader stack.
-     */
-    public function unregister()
-    {
-        spl_autoload_unregister(array($this, 'loadClass'));
-    }
+	public function unregister() {
+		spl_autoload_unregister(array($this, 'loadClass'));
+	}
 
-    /**
-     * Loads the given class or interface.
-     *
-     * @param string $className The name of the class to load.
-     * @return void
-     */
-    public function loadClass($className)
-    {
-        if (null === $this->namespace || $this->namespace.$this->namespaceSeparator === substr($className, 0, strlen($this->namespace.$this->namespaceSeparator))) {
-            $fileName = '';
+	public function loadClass($className) {
+		if (is_null($this->namespace) || $this->namespace . $this->namespaceSeparator === substr($className, 0, strlen($this->namespace . $this->namespaceSeparator))) {
+			$fileDirectoryPath = '';
+			$lastNsPos = strripos($className, $this->namespaceSeparator);
+			if ($lastNsPos !== false) {
+				$namespace = $this->getNamespaceFromClassName($className, $lastNsPos);
+				$shortClassName = $this->getShortClassName($className, $lastNsPos);
+				$fileDirectoryPath .= $this->namespaceToPath($namespace);
+			}
+			require_once $this->makeAbsolutePath($fileDirectoryPath, $shortClassName);
+		}
+	}
 
-            if (false !== ($lastNsPos = strripos($className, $this->namespaceSeparator))) {
-                $namespace = substr($className, 0, $lastNsPos);
-                $className = substr($className, $lastNsPos + 1);
-                $fileName = str_replace($this->namespaceSeparator, DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
-            }
-            $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . $this->fileExtension;
-            require_once ($this->includePath !== null ? $this->includePath . DIRECTORY_SEPARATOR : '') . $fileName;
-        }
-    }
+	private function namespaceToPath($namespace) {
+		return str_replace($this->namespaceSeparator, DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+	}
+
+	private function getShortClassName($className, $lastNsPos) {
+		return substr($className, $lastNsPos + 1);
+	}
+
+	private function getNamespaceFromClassName($className, $lastNsPos) {
+		return substr($className, 0, $lastNsPos);
+	}
+
+	private function makeAbsolutePath($fileDirectory, $shortClassName) {
+		return $this->includePath . DIRECTORY_SEPARATOR . $fileDirectory . $shortClassName . $this->fileExtension;
+	}
+
+	private function getLibraryPath() {
+		return __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR;
+	}
 }
