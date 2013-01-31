fb707a40d1fcca792d4b180e24c267f91f3f89de
Vadim Comanescu <vadim984@gmail.com>
2013-02-01 00:53:41 +0200

    Introduce the managed object concept.

---
+++ library/Vmwarephp/ManagedObject.php
@@ -0,0 +1,48 @@
+<?php
+namespace Vmwarephp;
+
+class ManagedObject {
+	private $reference;
+	protected $vmwareService;
+
+	function __construct(Service $vmwareService, ManagedObjectReference $managedObjectReference) {
+		$this->vmwareService = $vmwareService;
+		$this->reference = $managedObjectReference;
+	}
+
+	function getParentHost() {
+		return $this->vmwareService->getVhostHost();
+	}
+
+	function __get($propertyName) {
+		if (!isset($this->$propertyName)) {
+			$queryForProperty = 'get' . ucfirst($propertyName);
+			return $this->$queryForProperty();
+		}
+		return $this->$propertyName;
+	}
+
+	function __set($propertyName, $propertyValue) {
+		$this->$propertyName = $propertyValue;
+	}
+
+	function getReferenceType() {
+		return $this->reference->type;
+	}
+
+	function getReferenceId() {
+		return $this->reference->_;
+	}
+
+	function toReference() {
+		return $this->reference;
+	}
+
+	function __call($method, $arguments) {
+		return $this->vmwareService->$method($this, $arguments);
+	}
+
+	function equals(ManagedObject $managedObject) {
+		return $this->toReference() == $managedObject->toReference() && $this->getParentHost() == $managedObject->getParentHost();
+	}
+}
---
+++ tests/ManagedObjectTest.php
@@ -0,0 +1,36 @@
+<?php
+
+class ManagedObjectTest extends PHPUnit_Framework_TestCase {
+
+	private $vmwareService;
+	private $managedObject;
+
+	protected function setUp() {
+		$this->vmwareService = \Mockery::mock('Vmwarephp\Service');
+		$this->managedObject = new \Vmwarephp\ManagedObject($this->vmwareService, new \Vmwarephp\ManagedObjectReference('vm-115', 'VirtualMachine'));
+	}
+
+	function testIfPropertyIsSetOnTheObjectItNeverQueriesTheService() {
+		$this->managedObject->name = 'name';
+
+		$this->vmwareService->shouldReceive('getName')->never();
+
+		$this->assertEquals('name', $this->managedObject->name);
+	}
+
+	function testIfPropertyIsNotSetOnTheObjectItIsQueriedThroughTheService() {
+		$this->vmwareService->shouldReceive('getName')->with($this->managedObject, array());
+
+		$this->managedObject->name;
+	}
+
+	function testAMethodCallIsRedirectedToTheService() {
+		$this->vmwareService->shouldReceive('doSomething')->with($this->managedObject, array());
+
+		$this->managedObject->doSomething();
+	}
+
+	function testTransformsIntoAManagedObjectReference() {
+		$this->assertInstanceOf('\Vmwarephp\ManagedObjectReference', $this->managedObject->toReference());
+	}
+}
