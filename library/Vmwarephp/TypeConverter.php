abf4bb34581d06a27a981f09b9cc32f88dd0da07
Vadim Comanescu <vadim984@gmail.com>
2013-02-01 00:54:28 +0200

    Enable type conversion for values returned by the vmware service queries.

---
+++ library/Vmwarephp/TypeConverter.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Vmwarephp;
+
+class TypeConverter {
+	private $vmwareService;
+	private $managedObjectFactory;
+
+	function __construct(Service $service, \Vmwarephp\Factory\ManagedObject $managedObjectFactory = null) {
+		$this->vmwareService = $service;
+		$this->managedObjectFactory = $managedObjectFactory ? : new \Vmwarephp\Factory\ManagedObject();
+	}
+
+	function convert($value) {
+		if ($this->isANullValue($value)) return null;
+		if ($this->isABasicPrimitiveType($value)) return $value;
+		if ($this->isAManagedObjectReference($value)) return $this->managedObjectFactory->make($this->vmwareService, $value);
+		if ($this->isAnObjectContent($value)) return $this->convertObjectContent($value);
+		if (is_array($value)) return $this->convertEachValueOfTheArray($value);
+		if ($this->isAnArrayOf($value)) {
+			$elementType = $this->getArrayOfElementType($value);
+			return $this->convert(is_array($value->$elementType) ? $value->$elementType : array($value->$elementType));
+		}
+		return $this->convertDataObject($value);
+	}
+
+	private function convertObjectContent($objectContent) {
+		$managedObject = $this->convert($objectContent->obj);
+		$properties = $objectContent->propSet;
+		if (!$properties) return $managedObject;
+		$this->addPropertiesToObject($managedObject, is_array($properties) ? $properties : array($properties));
+		return $managedObject;
+	}
+
+	private function addPropertiesToObject($managedObject, $properties) {
+		foreach ($properties as $dynamicProperty) {
+			$propertyName = $dynamicProperty->name;
+			$managedObject->$propertyName = $this->convert($dynamicProperty->val);
+		}
+	}
+
+	private function convertEachValueOfTheArray($value) {
+		foreach ($value as $key => $val) $value[$key] = $this->convert($val);
+		return $value;
+	}
+
+	private function convertDataObject($value) {
+		$objProperties = get_object_vars($value);
+		foreach ($objProperties as $propertyName => $propertyValue) $value->$propertyName = $this->convert($propertyValue);
+		return $value;
+	}
+
+	private function isAnObjectContent($value) {
+		return $value instanceof ObjectContent;
+	}
+
+	private function isANullValue($value) {
+		if (is_null($value)) return true;
+		if (!is_object($value)) return false;
+		$objProperties = get_object_vars($value);
+		return count($objProperties) == 0;
+	}
+
+	private function isABasicPrimitiveType($value) {
+		return is_string($value) || is_double($value) || is_float($value) || is_int($value) || is_bool($value);
+	}
+
+	private function isAManagedObjectReference($value) {
+		if (!is_object($value)) return false;
+		return $value instanceof ManagedObjectReference || (isset($value->_) && isset($value->type));
+	}
+
+	private function isAnArrayOf($value) {
+		if (!is_object($value)) return false;
+		$objectClass = get_class($value);
+		return preg_match('/ArrayOf/', $objectClass) ? true : false;
+	}
+
+	private function getArrayOfElementType($value) {
+		$objVars = get_object_vars($value);
+		$objVarsKeys = array_keys($objVars);
+		return end($objVarsKeys);
+	}
+}
---
+++ tests/TypeConverterTest.php
@@ -0,0 +1,85 @@
+<?php
+
+class TypeConverterTest extends PHPUnit_Framework_TestCase {
+
+	private $typeConverter;
+	private $vmwareService;
+	private $factory;
+
+	protected function setUp() {
+		$this->vmwareService = \Mockery::mock('\Vmwarephp\Service');
+		$this->factory = new \Vmwarephp\Factory\ManagedObject();
+		$this->typeConverter = new \Vmwarephp\TypeConverter($this->vmwareService, $this->factory);
+	}
+
+	function testAnEmptyStdClassBecomesANullValue() {
+		$result = $this->typeConverter->convert(new stdClass());
+
+		$this->assertNull($result);
+	}
+
+	function testAPrimitiveBasicTypeRemainsABasicType() {
+		$basicTypesValues = array("value", true, 4, 5.6);
+
+		foreach ($basicTypesValues as $value)
+			$this->assertEquals($value, $this->typeConverter->convert($value));
+	}
+
+	function testAnArrayOfStringsTypeBecomesAPhpArrayOfStrings() {
+		$obj1 = new \Vmwarephp\ArrayOfString(array('1', '2'));
+		$obj2 = new \Vmwarephp\ArrayOfString('1');
+
+		$this->assertEquals(array('1', '2'), $this->typeConverter->convert($obj1));
+		$this->assertEquals(array('1'), $this->typeConverter->convert($obj2));
+	}
+
+	function testAManagedObjectReferenceBecomesAManagedObject() {
+		$reference = $this->factory->makeReference('datacenter-21', 'Datacenter');
+		$managedObject = $this->factory->make($this->vmwareService, $reference);
+
+		$this->assertEquals($managedObject, $this->typeConverter->convert($reference));
+	}
+
+	function testAnArrayOfManagedObjectReferenceTypeBecomesAPhpArrayOfManagedObjects() {
+		$reference = $this->factory->makeReference('datastore-82', 'Datastore');
+		$obj = new \Vmwarephp\ArrayOfManagedObjectReference($reference);
+
+		$expectedValue = array($this->factory->make($this->vmwareService, $reference));
+		$this->assertEquals($expectedValue, $this->typeConverter->convert($obj));
+	}
+
+	function testAnArrayOfAnyTypeBecomesAnArrayOfObjectsWithAllTheirPropertiesConverted() {
+		$alarmState1 = new \Vmwarephp\AlarmState("alarm-1.datacenter-21", $this->factory->makeReference('group-d1', 'Folder'));
+		$alarmState2 = new \Vmwarephp\AlarmState("alarm-2.datacenter-21", $this->factory->makeReference('group-d2', 'Folder'));
+
+		$anyTypeArray = new \Vmwarephp\ArrayOfAnyType(array($alarmState1, $alarmState2));
+
+		$alarmState1Converted = new \Vmwarephp\AlarmState("alarm-1.datacenter-21",
+			$this->factory->make($this->vmwareService, $this->factory->makeReference('group-d1', 'Folder')));
+		$alarmState2Converted = new \Vmwarephp\AlarmState("alarm-2.datacenter-21",
+			$this->factory->make($this->vmwareService, $this->factory->makeReference('group-d2', 'Folder')));
+
+		$convertedValue = array($alarmState1Converted, $alarmState2Converted);
+		$this->assertEquals($convertedValue, $this->typeConverter->convert($anyTypeArray));
+	}
+
+	// For a detailed definition of object content see sdk documentation: http://pubs.vmware.com/vsphere-51/index.jsp
+	function testAnObjectContentBecomesAManagedObjectWithProperties() {
+		$taskReference = $this->factory->makeReference('task-123', 'Task');
+		$dynamicProperty = new \Vmwarephp\DynamicProperty('recentTask', $taskReference);
+		$vmReference = $this->factory->makeReference('vm-114', 'VirtualMachine');
+		$objectContent = new \Vmwarephp\ObjectContent($vmReference, array($dynamicProperty));
+
+		$virtualMachine = $this->typeConverter->convert($objectContent);
+
+		$this->assertEquals($this->factory->make($this->vmwareService, $taskReference), $virtualMachine->recentTask);
+	}
+
+	function testAnObjectContentWithoutDynamicPropertyBecomesAManagedObjectWithoutAnyProperties() {
+		$vmReference = $this->factory->makeReference('vm-114', 'VirtualMachine');
+		$objectContent = new \Vmwarephp\ObjectContent($vmReference, null);
+
+		$virtualMachine = $this->typeConverter->convert($objectContent);
+		$this->assertEmpty(get_object_vars($virtualMachine));
+	}
+}
