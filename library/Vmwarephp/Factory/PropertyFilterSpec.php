d696183d3f72795318ae6ec35b80a95ae33aa44f
Vadim Comanescu <vadim984@gmail.com>
2013-02-19 00:43:50 +0200

    Introduce property filter spec factory to build the necessary specs for a future property collector.

---
+++ library/Vmwarephp/Factory/PropertyFilterSpec.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Vmwarephp\Factory;
+
+class PropertyFilterSpec {
+
+	function makeForOneManagedObject($managedObjectType, $referenceId, $propertiesToCollect) {
+		$sets = $this->makeTraversalAndPropSets($managedObjectType, $propertiesToCollect);
+		return new \PropertyFilterSpec($sets['propSet'], $this->makeObjectSet($managedObjectType, $referenceId, $sets['traversalSet']));
+	}
+
+	function makeForTraversingAllInventory($managedObjectType, $propertiesToCollect, \Vmwarephp\Service $service) {
+		$containerView = $this->makeContainerView($managedObjectType, $service);
+		$sets = $this->makeTraversalAndPropSets($managedObjectType, $propertiesToCollect);
+		$traversalSpec = $this->makeTraversalSpec('view', 'ContainerView');
+		$traversalSpec->selectSet = $sets['traversalSet'];
+		return new \PropertyFilterSpec($sets['propSet'], $this->makeObjectSet($containerView->type, $containerView->_, $traversalSpec));
+	}
+
+	private function makeTraversalAndPropSets($managedObjectType, $propertiesToCollect) {
+		$specs = array('propSet' => array(), 'traversalSet' => array());
+		if ($propertiesToCollect == 'all') {
+			$specs['propSet'][] = $this->makePropSpec($managedObjectType, $propertiesToCollect);
+			return $specs;
+		}
+		$nonTraversalProperties = array();
+		foreach ($propertiesToCollect as $key => $value) {
+			if ($this->isATraversalProperty($key)) {
+				$this->checkTraversalPropertyFormat($propertiesToCollect, $key);
+				$specs['traversalSet'][] = $this->makeTraversalSpec($key, $managedObjectType);
+				$specs['propSet'][] = $this->makePropSpec($propertiesToCollect[$key][0], $propertiesToCollect[$key][1]);
+			} else {
+				$nonTraversalProperties[] = $value;
+			}
+		}
+		$specs['propSet'][] = $this->makePropSpec($managedObjectType, $nonTraversalProperties);
+		return $specs;
+	}
+
+	private function makeContainerView($managedObjectType, $vmwareService) {
+		$viewManager = $vmwareService->getViewManager();
+		$containerView = $viewManager->CreateContainerView(
+			array('container' => $vmwareService->getRootFolder()->toReference(),
+				'recursive' => true, 'type' => $managedObjectType));
+		return $containerView->toReference();
+	}
+
+	private function makeTraversalSpec($key, $managedObjectType) {
+		return new \TraversalSpec('traverse' . ucfirst($key), $managedObjectType, $key, false, null);
+	}
+
+	private function makePropSpec($managedObjectType, $propertiesToBeRetrieved) {
+		if ($propertiesToBeRetrieved == 'all') return new \PropertySpec($managedObjectType, true);
+		return new \PropertySpec($managedObjectType, false, $propertiesToBeRetrieved);
+	}
+
+	private function isATraversalProperty($key) {
+		return !is_numeric($key);
+	}
+
+	private function makeObjectSet($managedObjectType, $referenceId, $traversalSpecsSelectSet = array()) {
+		return array(new \ObjectSpec(new \ManagedObjectReference($referenceId, $managedObjectType), true, $traversalSpecsSelectSet));
+	}
+
+	private function checkTraversalPropertyFormat($propertiesToCollect, $key) {
+		if (!isset($propertiesToCollect[$key][0]) || !isset($propertiesToCollect[$key][1]))
+			throw new \Vmwarephp\Exception\InvalidTraversalPropertyFormat();
+	}
+}
---
+++ tests/Factory/PropertyFilterSpec.php
@@ -0,0 +1,86 @@
+<?php
+
+class PropertyFilterSpecTest extends PHPUnit_Framework_TestCase {
+
+	private $factory;
+
+	protected function setUp() {
+		$this->factory = new \Vmwarephp\Factory\PropertyFilterSpec();
+	}
+
+	function testMakesAPropertyFilterSpecForGatheringAListOrPropertiesForAManagedObject() {
+		$requestedProperties = array('configStatus');
+		$referenceId = 'vm-151';
+		$managedObjectType = 'VirtualMachine';
+
+		$propSet = array(new \PropertySpec($managedObjectType, false, $requestedProperties));
+		$objectSet = array(new \ObjectSpec(new \ManagedObjectReference($referenceId, $managedObjectType), true, array()));
+
+		$expectedPropertyFilterSpec = new \PropertyFilterSpec($propSet, $objectSet);
+		$this->assertEquals($expectedPropertyFilterSpec, $this->factory->makeForOneManagedObject($managedObjectType, $referenceId, $requestedProperties));
+	}
+
+	function testMakesAPropertyFilterSpecForGatheringAllPropertiesForAManagedObject() {
+		$managedObjectType = 'VirtualMachine';
+		$requestedProperties = 'all';
+		$referenceId = 'vm-151';
+
+		$propSet = array(new \PropertySpec($managedObjectType, true));
+		$objectSet = array(new \ObjectSpec(new \ManagedObjectReference($referenceId, $managedObjectType), true, array()));
+
+		$expectedPropertyFilterSpec = new \PropertyFilterSpec($propSet, $objectSet);
+
+		$this->assertEquals($expectedPropertyFilterSpec, $this->factory->makeForOneManagedObject($managedObjectType, $referenceId, $requestedProperties));
+	}
+
+	function testMakesAPropertyFilterSpecForGatheringTraversalPropertiesForAManagedObject() {
+		# Gather vm configStatus, go to network and gather its configStatus
+		$managedObjectType = 'VirtualMachine';
+		$requestedProperties = array('configStatus', 'network' => array('Network', array('configStatus')));
+		$referenceId = 'vm-151';
+
+		$traversalSpecs = array(new \TraversalSpec('traverseNetwork', $managedObjectType, 'network', false));
+		$propSet = array(new \PropertySpec('Network', false, array('configStatus')), new \PropertySpec($managedObjectType, false, array('configStatus')));
+		$objectSet = array(new \ObjectSpec(new \ManagedObjectReference($referenceId, $managedObjectType), true, $traversalSpecs));
+
+		$expectedPropertyFilterSpec = new \PropertyFilterSpec($propSet, $objectSet);
+		$this->assertEquals($expectedPropertyFilterSpec, $this->factory->makeForOneManagedObject($managedObjectType, $referenceId, $requestedProperties));
+	}
+
+	function testMakesAPropertyFilterSpecForTraversingAllInventoryForAGivenManagedObjectAndItsProperties() {
+		$requestedProperties = array('configStatus');
+		$managedObjectType = 'VirtualMachine';
+
+		$vmwareService = \Mockery::mock('\Vmwarephp\Service');
+		$aContainerView = $this->mockContainerViewUsingService($vmwareService, $managedObjectType);
+		$containerViewReference = $aContainerView->toReference();
+
+		$propSet = array(new \PropertySpec($managedObjectType, false, $requestedProperties));
+		$traversalSpec = new \TraversalSpec('traverseView', 'ContainerView', 'view', false, array());
+		$objectSet = array(new \ObjectSpec($containerViewReference, true, $traversalSpec));
+
+		$expectedPropertyFilterSpec = new \PropertyFilterSpec($propSet, $objectSet);
+		$this->assertEquals($expectedPropertyFilterSpec, $this->factory->makeForTraversingAllInventory($managedObjectType, $requestedProperties, $vmwareService));
+	}
+
+	private function mockContainerViewUsingService($vmwareService, $managedObjectType) {
+		$aContainerManager = $this->aManagedObject();
+		$vmwareService->shouldReceive('getViewManager')->once()->andReturn($aContainerManager);
+		$rootFolder = $this->aManagedObjectThatExpectsToBeCastToReference();
+		$vmwareService->shouldReceive('getRootFolder')->once()->andReturn($rootFolder);
+		$aContainerView = $this->aManagedObjectThatExpectsToBeCastToReference(new ManagedObjectReference('1', 'ContainerView'));
+		$aContainerManager->shouldReceive('CreateContainerView')->with(array('container' => $rootFolder->toReference(),
+			'recursive' => true, 'type' => $managedObjectType))->andReturn($aContainerView);
+		return $aContainerView;
+	}
+
+	private function aManagedObjectThatExpectsToBeCastToReference($reference = null) {
+		$managedObject = $this->aManagedObject();
+		$managedObject->shouldReceive('toReference')->atLeast($this->once())->andReturn($reference);
+		return $managedObject;
+	}
+
+	private function aManagedObject() {
+		return \Mockery::mock('\Vmwarephp\ManagedObject');
+	}
+}
