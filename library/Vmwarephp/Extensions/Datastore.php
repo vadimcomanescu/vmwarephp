58b522e3bd0fa7a0fa933c1ae7de613781ded81e
Vadim Comanescu <vadim984@gmail.com>
2013-03-07 22:38:55 +0200

    Introduce Datastore managed object extension

---
+++ library/Vmwarephp/Extensions/Datastore.php
@@ -0,0 +1,41 @@
+<?php
+namespace Vmwarephp\Extensions;
+
+class Datastore extends \Vmwarephp\ManagedObject {
+
+	function expandToMaxSize() {
+		try {
+			$this->RefreshDatastore();
+			if ($this->isANfsDatastore()) return;
+			foreach ($this->getConnectedHosts() as $host)
+				$host->expandDatastore($this);
+		} catch (\Exception $e) {
+			return;
+		}
+	}
+
+	function getConnectedHosts() {
+		$hosts = array();
+		foreach ($this->host as $hostMount)
+			if ($hostMount) $hosts[] = $hostMount->key;
+		return $hosts;
+	}
+
+	function getVirtualMachinesReferencingThisDatastore() {
+		if (!$this->vm[0]) return array();
+		return array_filter($this->vm, function ($aVm) {
+			return !$aVm->isTemplate();
+		});
+	}
+
+	function getVirtualMachinesInstalledOnThisDatastore() {
+		$vms = array();
+		foreach ($this->getVirtualMachinesReferencingThisDatastore() as $vm)
+			if ($vm->getParentDatastoreName() == $this->name) $vms[] = $vm;
+		return $vms;
+	}
+
+	function isAccessible() {
+		return in_array($this->configStatus, array('green', 'gray'));
+	}
+}
