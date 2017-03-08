c6bebec9d4f90d7b27e4cc57a8e3f14dbe21913a
Luke Tworek <ltworek@singlehop.com>
2017-03-07 21:17:25 -0500

    forcing us to allow the usage of self signed certificates

--- library/Vmwarephp/Factory/SoapClient.php
+++ library/Vmwarephp/Factory/SoapClient.php
@@ -3,41 +3,50 @@ namespace Vmwarephp\Factory;
 use \Vmwarephp\Exception as Ex;
 
 class SoapClient {
-	private $wsdlClassMapper;
-	private $wsdlFilePath;
+    private $wsdlClassMapper;
+    private $wsdlFilePath;
 
-	function __construct(\Vmwarephp\WsdlClassMapper $mapper = null, $wsdlFilePath = null) {
-		$this->wsdlClassMapper = $mapper ? : new \Vmwarephp\WsdlClassMapper;
-		$this->wsdlFilePath = $wsdlFilePath ? : $this->getWsdlFilePath();
-	}
+    function __construct(\Vmwarephp\WsdlClassMapper $mapper = null, $wsdlFilePath = null) {
+        $this->wsdlClassMapper = $mapper ? : new \Vmwarephp\WsdlClassMapper;
+        $this->wsdlFilePath = $wsdlFilePath ? : $this->getWsdlFilePath();
+    }
 
-	function make(\Vmwarephp\Vhost $vhost, $useExceptions = 1, $trace = 1) {
-		$options = array(
-			'trace' => $trace,
-			'location' => $this->makeRequestsLocation($vhost),
-			'exceptions' => $useExceptions,
-			'connection_timeout' => 10,
-			'classmap' => $this->wsdlClassMapper->getClassMap(),
-			'features' => SOAP_SINGLE_ELEMENT_ARRAYS + SOAP_USE_XSI_ARRAY_TYPE
-		);
-		$soapClient = $this->makeDefaultSoapClient($this->wsdlFilePath, $options);
-		if (!$soapClient) throw new Ex\CannotCreateSoapClient();
-		return $soapClient;
-	}
+    function make(\Vmwarephp\Vhost $vhost, $useExceptions = 1, $trace = 1) {
+        $context = stream_context_create(array(
+            'ssl' => array(
+                'verify_peer' => false,
+                'verify_peer_name' => false,
+                'allow_self_signed' => true
+            )
+        ));
 
-	function getClientClassMap() {
-		return $this->wsdlClassMapper->getClassMap();
-	}
+        $options = array(
+            'trace' => $trace,
+            'location' => $this->makeRequestsLocation($vhost),
+            'exceptions' => $useExceptions,
+            'connection_timeout' => 120,
+            'classmap' => $this->wsdlClassMapper->getClassMap(),
+            'features' => SOAP_SINGLE_ELEMENT_ARRAYS + SOAP_USE_XSI_ARRAY_TYPE,
+            'stream_context' => $context
+        );
+        $soapClient = $this->makeDefaultSoapClient($this->wsdlFilePath, $options);
+        if (!$soapClient) throw new Ex\CannotCreateSoapClient();
+        return $soapClient;
+    }
 
-	protected function makeRequestsLocation(\Vmwarephp\Vhost $vhost) {
-		return 'https://' . $vhost->host . '/sdk';
-	}
+    function getClientClassMap() {
+        return $this->wsdlClassMapper->getClassMap();
+    }
 
-	protected function makeDefaultSoapClient($wsdl, array $options) {
-		return @new \Vmwarephp\SoapClient($wsdl, $options);
-	}
+    protected function makeRequestsLocation(\Vmwarephp\Vhost $vhost) {
+        return 'https://' . $vhost->host . '/sdk';
+    }
 
-	private function getWsdlFilePath() {
-		return __DIR__ . '/../Wsdl/vimService.wsdl';
-	}
+    protected function makeDefaultSoapClient($wsdl, array $options) {
+        return @new \Vmwarephp\SoapClient($wsdl, $options);
+    }
+
+    private function getWsdlFilePath() {
+        return __DIR__ . '/../Wsdl/vimService.wsdl';
+    }
 }
